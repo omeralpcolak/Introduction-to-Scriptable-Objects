6da1a4f18a319e188f9821c5e92a465300596927
omeralpcolak <omer.alp.colak@gmail.com>
2023-02-07 15:50:29 +0300

    Create an asset menu

--- ScriptableObjects/Assets/Example1/MyScriptableObjectClass.cs
+++ ScriptableObjects/Assets/Example1/MyScriptableObjectClass.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Data", menuName = "Inventory/List", order = 1)]
 public class MyScriptableObjectClass : ScriptableObject
-{
+{   
+    
     public string objectName = "New MyScritableObject";
     public bool colorIsRandom = false;
     public Color thisColor = Color.white;
